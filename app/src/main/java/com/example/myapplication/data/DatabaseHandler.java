c41b296337a74fe6454a7180a25682597c06b4a8
Bakyt <djumabaevb@gmail.com>
2021-03-30 14:53:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/data/DatabaseHandler.java
@@ -0,0 +1,50 @@
+package com.example.myapplication.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.myapplication.R;
+import com.example.myapplication.model.Contact;
+import com.example.myapplication.util.Util;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        // create table_name(id, name, phone_number);
+
+        String CREATE_CONTACT_TABLE = "Create table " + Util.TABLE_NAME + "(" + Util.KEY_ID +
+                " INTEGER PRIMARY KEY, " + Util.KEY_NAME + " TEXT, " + Util.KEY_PHONE_NUMBER +
+                " TEXT" + ")";
+        db.execSQL(CREATE_CONTACT_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        String DROP_TABLE = String.valueOf(R.string.drop_table);
+        db.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
+
+        onCreate(db);
+    }
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Util.KEY_NAME, contact.getName());
+        values.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        db.insert(Util.TABLE_NAME, null, values);
+
+    }
+}
