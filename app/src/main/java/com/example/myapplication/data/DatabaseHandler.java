6c5d0da9453b1fd68247d0d5f23d761e53772553
Bakyt <djumabaevb@gmail.com>
2021-03-30 15:51:02 +0600

    using cursor to get an object - contact

--- app/src/main/java/com/example/myapplication/data/DatabaseHandler.java
+++ app/src/main/java/com/example/myapplication/data/DatabaseHandler.java
@@ -57,5 +57,15 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         Cursor cursor = db.query(Util.TABLE_NAME, new String[]{Util.KEY_ID, Util.KEY_NAME, Util.KEY_PHONE_NUMBER},
                 Util.KEY_ID + "=?", new String[]{String.valueOf(id)}, null, null, null);
 
+        if(cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        Contact contact = new Contact();
+        contact.setId(Integer.parseInt(cursor.getString(0)));
+        contact.setName((cursor.getString(1)));
+        contact.setPhoneNumber(cursor.getString(2));
+
+        return contact;
     }
 }
