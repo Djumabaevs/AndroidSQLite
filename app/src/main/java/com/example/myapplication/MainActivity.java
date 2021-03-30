2f516989ab827200bc74f611f81c592ee74bc3b9
Bakyt <djumabaevb@gmail.com>
2021-03-30 16:36:46 +0600

    db activated

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.myapplication.data.DatabaseHandler;
+import com.example.myapplication.model.Contact;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler db = new DatabaseHandler(MainActivity.this);
+
+        Contact Jeremy = new Contact();
+        Jeremy.setName("Jeremy");
+        Jeremy.setPhoneNumber("123456789");
+
+        db.addContact(Jeremy);
+
+        List<Contact> contactList = db.getAllContacts();
+        for( Contact contact : contactList) {
+            Log.d("Main", "onCreate: " + contact.getName());
+        }
     }
 }
