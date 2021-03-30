e8eb0586336976dbd05d24c930501d8a387c7fe3
Bakyt <djumabaevb@gmail.com>
2021-03-30 15:30:14 +0600

    add method get and add

--- app/src/main/java/com/example/myapplication/data/DatabaseHandler.java
+++ app/src/main/java/com/example/myapplication/data/DatabaseHandler.java
@@ -2,6 +2,7 @@ package com.example.myapplication.data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -37,6 +38,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         onCreate(db);
     }
+
     public void addContact(Contact contact) {
         SQLiteDatabase db = this.getWritableDatabase();
 
@@ -45,6 +47,15 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         values.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
 
         db.insert(Util.TABLE_NAME, null, values);
+        db.close();
+
+    }
+
+    public Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Util.TABLE_NAME, new String[]{Util.KEY_ID, Util.KEY_NAME, Util.KEY_PHONE_NUMBER},
+                Util.KEY_ID + "=?", new String[]{String.valueOf(id)}, null, null, null);
 
     }
 }
